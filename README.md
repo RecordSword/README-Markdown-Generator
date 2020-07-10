8df5e79db265f0e3c5b2f26654fa5c04b7d30540
Nick  Sword <nicksword@hotmail.com>
2020-07-10 13:39:01 +0800

    Generator completed, video created and added

---
+++ README.md
@@ -0,0 +1,14 @@
+# README Generator
+A program that gives the user prompts in order to generate a README for all their programs. This will be a useful too in order to have a user create their readme's in a uniform manner and ensure they do not forget any information.
+## Installation
+npm install
+## How to use the Application
+The application prompts the user with questions from a Node package. The application then creates and appends the responses from the user to a document called README.md. This is a generic document that most README's utilise.      
+## Other Contibuting Developers:
+none
+## Licence(s)
+none
+This application was developed by: RecordSword
+
+## Recording of Video showing code at work
+<iframe src="https://drive.google.com/file/d/1k9RzXFIbB0PqpnxcF8JDuqPpZcLz6Gs7/preview" width="640" height="480"></iframe>
--- index.js
+++ index.js
@@ -2,10 +2,159 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // Array for the questions
-const questions = [];
+const questions = [
+    //ask user for name of the app 
+    {
+        type: 'input',
+        name: 'titleApp',
+        message: 'What is title of this application? (This will also the main header of the README)',
 
-// Creates the README and appends the user input
-fs.appendFileSync("README.md")
+    },
+    // asks for a description of the project 
+    {
+        type: 'input',
+        message: 'Write a description of this application',
+        name: 'description',
+
+    },
+    // How do you install your app? 
+    {
+        type: 'input',
+        message: 'How does a user install the application? Provide instuctions here',
+        name: 'installApp',
+
+    },
+    // How does a user use your application? 
+    {
+        type: 'input',
+        message: 'How does the application work? Provide instuctions here',
+        name: 'appInstructions',
+
+    },
+    // What license does the user have? 
+    {
+        type: 'input',
+        message: 'What license they would you  like to use? (Ex: MIT, GPL 3.0) If you do not wish to use a license type "none" ',
+        name: 'license',
+
+    },
+
+    {
+        name: 'badge',
+        message: 'Do you want a standard-readme compliant badge?',
+        type: 'confirm',
+        default: true
+    },
+    //Asks who else contributed to their application 
+    {
+        type: 'input',
+        message: 'Who else contributed to your application? if none write "none" ',
+        name: 'contributors',
+
+    },
+    //asks user for git hub user name 
+    {
+        type: 'input',
+        name: 'gitHubUserName',
+        message: 'What is your username on Github?',
+
+    },
+];
+
+function init() {
+    inquirer.prompt(questions).then((response) => {
+
+        // Creates the README and appends the user input
+        fs.appendFileSync("README.md", ("# " + response.titleApp) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+        })
+        // adds the discription of the project that the users entered 
+        fs.appendFileSync("README.md", (response.description) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        //creates a sub header for installation and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## Installation" + '\n' + response.installApp) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for App usage and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## How to use the Application" + '\n' + response.appInstructions) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for issue reporting and contriubiting and adds the instructions the users entered 
+        fs.appendFileSync("README.md", ("## Other Contibuting Developers:" + '\n' + response.contributors) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // creates a sub header for licences and adds the licences the users entered 
+        fs.appendFileSync("README.md", ("## Licence(s)" + '\n' + response.license) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+        // this will create the 2nd line of readme discribing the application was developed by the users reponse to gitHubUserName 
+        fs.appendFileSync("README.md", ("This application was developed by: " + response.gitHubUserName + '\n') + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+
+    })
+}
 
 //runs the function to create the file 
 init();
+
+//for testing purposes: 
+
+//Shows what the response for the questions were. 
+// console.log(response)
+// console.log(response.gitHubUserName)
+// console.log(response.titleApp)
+// console.log(response.description)
+// console.log(response.appInstructions)
+// console.log(response.appIssuesAndCont)
+// console.log(response.license)
+// console.log(response.contributors)
